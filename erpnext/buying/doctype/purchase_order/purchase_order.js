b8eae08344633545eff9e41b3867f514a9692ac8
lovinmaxwell <lovinmaxwell@gmail.com>
2021-09-14 08:51:56 +0000

    For ETS Removed the Update Item from PO and SO

--- erpnext/buying/doctype/purchase_order/purchase_order.js
+++ erpnext/buying/doctype/purchase_order/purchase_order.js
@@ -143,16 +143,16 @@ erpnext.buying.PurchaseOrderController = erpnext.buying.BuyingController.extend(
 			}
 
 			if(!in_list(["Closed", "Delivered"], doc.status)) {
-				if(this.frm.doc.status !== 'Closed' && flt(this.frm.doc.per_received) < 100 && flt(this.frm.doc.per_billed) < 100) {
-					this.frm.add_custom_button(__('Update Items'), () => {
-						erpnext.utils.update_child_items({
-							frm: this.frm,
-							child_docname: "items",
-							child_doctype: "Purchase Order Detail",
-							cannot_add_row: false,
-						})
-					});
-				}
+				// if(this.frm.doc.status !== 'Closed' && flt(this.frm.doc.per_received) < 100 && flt(this.frm.doc.per_billed) < 100) {
+				// 	this.frm.add_custom_button(__('Update Items'), () => {
+				// 		erpnext.utils.update_child_items({
+				// 			frm: this.frm,
+				// 			child_docname: "items",
+				// 			child_doctype: "Purchase Order Detail",
+				// 			cannot_add_row: false,
+				// 		})
+				// 	});
+				// }
 				if (this.frm.has_perm("submit")) {
 					if(flt(doc.per_billed, 6) < 100 || flt(doc.per_received, 6) < 100) {
 						if (doc.status != "On Hold") {
--- erpnext/selling/doctype/sales_order/sales_order.js
+++ erpnext/selling/doctype/sales_order/sales_order.js
@@ -45,17 +45,17 @@ frappe.ui.form.on("Sales Order", {
 		});
 	},
 	refresh: function(frm) {
-		if(frm.doc.docstatus === 1 && frm.doc.status !== 'Closed'
-			&& flt(frm.doc.per_delivered, 6) < 100 && flt(frm.doc.per_billed, 6) < 100) {
-			frm.add_custom_button(__('Update Items'), () => {
-				erpnext.utils.update_child_items({
-					frm: frm,
-					child_docname: "items",
-					child_doctype: "Sales Order Detail",
-					cannot_add_row: false,
-				})
-			});
-		}
+		// if(frm.doc.docstatus === 1 && frm.doc.status !== 'Closed'
+		// 	&& flt(frm.doc.per_delivered, 6) < 100 && flt(frm.doc.per_billed, 6) < 100) {
+		// 	frm.add_custom_button(__('Update Items'), () => {
+		// 		erpnext.utils.update_child_items({
+		// 			frm: frm,
+		// 			child_docname: "items",
+		// 			child_doctype: "Sales Order Detail",
+		// 			cannot_add_row: false,
+		// 		})
+		// 	});
+		// }
 	},
 	onload: function(frm) {
 		if (!frm.doc.transaction_date){
