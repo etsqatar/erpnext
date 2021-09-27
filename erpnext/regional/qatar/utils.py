ba1b762fa6aaebb00ed33e0799e3955845bbaf28
Lovin Maxwell <lovinmaxwell@gmail.com>
2021-09-27 11:28:33 +0300

    Anurag810/erpnext/tree/wps_report

--- erpnext/regional/qatar/utils.py
+++ erpnext/regional/qatar/utils.py
@@ -251,33 +251,4 @@ def get_employee_data(employee):
 	return frappe.db.get_value("Employee", employee, ["residential_id", "visa_id", "employee_name",
 		"bank_abbr", "bank_ac_no"], as_dict=1)
 
-def validate_iban(iban):
-	'''
-	Algorithm: https://en.wikipedia.org/wiki/International_Bank_Account_Number#Validating_the_IBAN
-	'''
-	# IBAN field is optional
-	if not iban:
-		return
-
-	def encode_char(c):
-		# Position in the alphabet (A=1, B=2, ...) plus nine
-		return str(9 + ord(c) - 64)
-
-	# remove whitespaces, upper case to get the right number from ord()
-	iban = ''.join(iban.split(' ')).upper()
-
-	# Move country code and checksum from the start to the end
-	flipped = iban[4:] + iban[:4]
-
-	# Encode characters as numbers
-	encoded = [encode_char(c) if ord(c) >= 65 and ord(c) <= 90 else c for c in flipped]
-
-	try:
-		to_check = int(''.join(encoded))
-	except ValueError:
-		frappe.throw(_('IBAN is not valid'))
-
-	if to_check % 97 != 1:
-		frappe.throw(_('IBAN is not valid'))
-
 
